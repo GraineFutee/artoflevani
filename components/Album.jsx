ff7da276ba9edf689c283f1fff0eb69aa2f5f95d
GraineFutee <pierre.asdrubal@gmail.com>
2020-06-26 18:40:29 +0200

    changes on picture size in modal

--- components/Album.jsx
+++ components/Album.jsx
@@ -52,7 +52,7 @@ export default function Album(props) {
           }}
         >
           <p className="title has-text-white">{props.album}</p>
-          <p>{props.pictures} Pictures</p>
+          <p className="title has-text-white">{props.pictures} Pictures</p>
         </div>
       </figure>
     </Link>
